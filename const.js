91f8ae74f5ada73e9065ce905270f3430cb1c36e
RoyMartinez <roy.martinez@axxis-systems.com>
2023-10-27 15:50:24 -0600

    :rotating_light: fix all the linter warnings

--- const.js
+++ const.js
@@ -1,19 +1,17 @@
+export const BLOCK_SIZE = 20
+export const BOARD_WIDTH = 14
+export const BOARD_HEIGHT = 30
+export const TIME_TO_DROP = 500
 
-export const BLOCK_SIZE = 20;
-export const BOARD_WIDTH = 14;
-export const BOARD_HEIGHT = 30;
-export const TIME_TO_DROP = 500;
-
-
-export const MOVEMENTS ={
-    LEFT:'ArrowLeft',
-    RIGHT:'ArrowRight',
-    DOWN:'ArrowDown',
-    UP:'ArrowUp'
+export const MOVEMENTS = {
+  LEFT: 'ArrowLeft',
+  RIGHT: 'ArrowRight',
+  DOWN: 'ArrowDown',
+  UP: 'ArrowUp'
 }
 
-export const COLOURS ={
-    BOARD:'#000',
-    PIECE:'red',
-    SOLIDIFIED_PIECE:'yellow',
+export const COLOURS = {
+  BOARD: '#000',
+  PIECE: 'red',
+  SOLIDIFIED_PIECE: 'yellow'
 }
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
-import {BLOCK_SIZE,BOARD_HEIGHT,BOARD_WIDTH,TIME_TO_DROP,MOVEMENTS,COLOURS} from './const'
-
+import { BLOCK_SIZE, BOARD_HEIGHT, BOARD_WIDTH, TIME_TO_DROP, MOVEMENTS, COLOURS } from './const'
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
@@ -11,206 +10,199 @@ let lastTime = 0
 let dropCounter = 0
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
-context.scale(BLOCK_SIZE,BLOCK_SIZE)
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
 
-const board = createBoard(BOARD_WIDTH,BOARD_HEIGHT)
+const board = createBoard(BOARD_WIDTH, BOARD_HEIGHT)
 const piecesShapes = [
   [
-    [1,1],
-    [1,1]
+    [1, 1],
+    [1, 1]
   ],
   [
-    [0,1,1],
-    [1,1,0]
+    [0, 1, 1],
+    [1, 1, 0]
   ],
   [
-    [1,1,1],
-    [0,1,0]
+    [1, 1, 1],
+    [0, 1, 0]
   ],
   [
-    [1,1,1,1]
+    [1, 1, 1, 1]
   ],
   [
-    [1,1,0],
-    [0,1,1]
+    [1, 1, 0],
+    [0, 1, 1]
   ],
   [
-    [1,0,0],
-    [1,1,1]
+    [1, 0, 0],
+    [1, 1, 1]
   ],
   [
-    [0,0,1],
-    [1,1,1]
+    [0, 0, 1],
+    [1, 1, 1]
   ]
 ]
 
-const piece ={
-  position :{x:5,y:5},
+const piece = {
+  position: { x: 5, y: 5 },
   shape: getShape()
 }
 
-function getShape(){
-  return piecesShapes[Math.floor(Math.random()* piecesShapes.length)]
+function getShape () {
+  return piecesShapes[Math.floor(Math.random() * piecesShapes.length)]
 }
 
-function createBoard(width, height){
-  return Array(height).fill().map(()=> Array(width).fill(0))
+function createBoard (width, height) {
+  return Array(height).fill().map(() => Array(width).fill(0))
 }
 
-
-
-function drawBackGround(){
-  context.fillStyle= COLOURS.BOARD;
-  context.fillRect(0,0,canvas.width,canvas.height)
+function drawBackGround () {
+  context.fillStyle = COLOURS.BOARD
+  context.fillRect(0, 0, canvas.width, canvas.height)
 }
-function drawSolidifiedPieces(){
-  board.forEach((row,y)=>{
-    row.forEach((value,x)=>{
-       if(value === 1){
-          context.fillStyle = COLOURS.SOLIDIFIED_PIECE
-          context.fillRect(x,y,1,1)
-       }
+function drawSolidifiedPieces () {
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = COLOURS.SOLIDIFIED_PIECE
+        context.fillRect(x, y, 1, 1)
+      }
     })
   })
 }
-function drawPiece(){
-  piece.shape.forEach((row,y)=>{
-    row.forEach((value,x)=>{
-        if(value ==1){
-          context.fillStyle =COLOURS.PIECE
-          context.fillRect(x+piece.position.x,y+piece.position.y,1,1)
-        }
+function drawPiece () {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = COLOURS.PIECE
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+      }
     })
   })
 }
 
-function draw(){
+function draw () {
   drawBackGround()
   drawSolidifiedPieces()
   drawPiece()
 }
 
-function checkCollision(){
-  return piece.shape.find((row,y)=>{
-    return row.find((value,x)=>{
-      return(
+function checkCollision () {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
         value !== 0 &&
-        board[y+piece.position.y]?.[x+piece.position.x]!== 0
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
       )
     })
   })
 }
 
-function checkGameOver(){
-  if(checkCollision()){
-    window.alert("Game Over")
+function checkGameOver () {
+  if (checkCollision()) {
+    window.alert('Game Over')
     board.forEach(row => row.fill(0))
   }
 }
 
-function restartPiecePosition(){
-  //get random shape and randon position
+function restartPiecePosition () {
+  // get random shape and randon position
   piece.shape = getShape()
-  //get random position
-  piece.position.x = Math.floor(Math.random() * BOARD_WIDTH/2)
+  // get random position
+  piece.position.x = Math.floor(Math.random() * BOARD_WIDTH / 2)
   piece.position.y = 0
-  
+
   checkGameOver()
 }
 
-function solidifyPiece(){
-  piece.shape.forEach((row,y)=>{
-    row.forEach((value,x)=>{
-        if(value === 1){
-          board[y+piece.position.y][x+piece.position.x] = 1
-        }
+function solidifyPiece () {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1
+      }
     })
   })
-  restartPiecePosition();
+  restartPiecePosition()
 }
 
-function removeRows(){
+function removeRows () {
   const rowsToRemove = []
 
-  board.forEach((row,y)=>{
-    if(row.every(value => value === 1)){
-      rowsToRemove.push(y);
+  board.forEach((row, y) => {
+    if (row.every(value => value === 1)) {
+      rowsToRemove.push(y)
     }
   })
 
-  rowsToRemove.forEach(y=>{
-    score +=10
-    board.splice(y,1)
+  rowsToRemove.forEach(y => {
+    score += 10
+    board.splice(y, 1)
     const newRow = Array(BOARD_WIDTH).fill(0)
     board.unshift(newRow)
   })
   $score.innerText = score
 }
 
-
-function moveLeft(){
+function moveLeft () {
   piece.position.x--
-  if(checkCollision())
-    piece.position.x++
+  if (checkCollision()) { piece.position.x++ }
 }
-function moveRigth(){
+function moveRigth () {
   piece.position.x++
-  if(checkCollision())
-    piece.position.x--
+  if (checkCollision()) { piece.position.x-- }
 }
-function moveDown(){
-  piece.position.y++;
-  if(checkCollision()){
+function moveDown () {
+  piece.position.y++
+  if (checkCollision()) {
     piece.position.y--
     solidifyPiece()
     removeRows()
   }
 }
-function moveUp(){
+function moveUp () {
   const rotatedShape = []
-  for(let i = 0; i < piece.shape[0].length;i++){
-    const row =[]
-    for(let j = piece.shape.length-1;j>=0;j--){
+  for (let i = 0; i < piece.shape[0].length; i++) {
+    const row = []
+    for (let j = piece.shape.length - 1; j >= 0; j--) {
       row.push(piece.shape[j][i])
     }
     rotatedShape.push(row)
   }
   const originalShape = piece.shape
   piece.shape = rotatedShape
-  if(checkCollision()){
+  if (checkCollision()) {
     piece.shape = originalShape
   }
 }
 
-
-document.addEventListener('keydown',event=>{
-  if(event.key===MOVEMENTS.LEFT) moveLeft()
-  if(event.key===MOVEMENTS.RIGHT) moveRigth()
-  if(event.key===MOVEMENTS.DOWN) moveDown()
-  if(event.key === MOVEMENTS.UP) moveUp()
+document.addEventListener('keydown', event => {
+  if (event.key === MOVEMENTS.LEFT) moveLeft()
+  if (event.key === MOVEMENTS.RIGHT) moveRigth()
+  if (event.key === MOVEMENTS.DOWN) moveDown()
+  if (event.key === MOVEMENTS.UP) moveUp()
 })
 
-
-function drop(time){
-  const deltaTime = time-lastTime
+function drop (time) {
+  const deltaTime = time - lastTime
   lastTime = time
   dropCounter += deltaTime
 
-  if(dropCounter>TIME_TO_DROP){
+  if (dropCounter > TIME_TO_DROP) {
     piece.position.y++
-    dropCounter=0
+    dropCounter = 0
     moveDown()
   }
 }
 
 // el refrescamiento del juego
-function update(time = 0){
+function update (time = 0) {
   drop(time)
   draw()
-  requestAnimationFrame(update)
+  window.requestAnimationFrame(update)
 }
 
-function start(){
+function start () {
   update()
 }
 
