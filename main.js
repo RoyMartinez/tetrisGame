d5698404a6189ddba5496b732f0ccba628e2402b
RoyMartinez <roy.martinez@axxis-systems.com>
2023-10-27 12:20:16 -0600

    :sparkles: feature, add game over

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const context = canvas.getContext('2d')
 const BLOCK_SIZE = 20;
 const BOARD_WIDTH = 14;
 const BOARD_HEIGH = 30;
+const TIME_TO_DROP = 500;
 
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGH
@@ -55,23 +56,46 @@ const piece ={
   ]
 }
 
+const piecesShapes = [
+  [
+    [1,1],
+    [1,1]
+  ],
+  [
+    [0,1,1],
+    [1,1,0]
+  ],
+  [
+    [1,1,1],
+    [0,1,0]
+  ],
+  [
+    [1,1,1,1]
+  ],
+  [
+    [1,1,0],
+    [0,1,1]
+  ],
+]
+
 
 let lastTime = 0
 let dropCounter = 0
 // el refrescamiento del juego
 function update(time = 0){
+  
   const deltaTime = time-lastTime
   lastTime = time
   dropCounter += deltaTime
 
-  if(dropCounter>1000){
+  if(dropCounter>TIME_TO_DROP){
     piece.position.y++
+    dropCounter =0
     if(checkCollision()){
       piece.position.y--
       solidifyPiece()
       removeRows()
     }
-    dropCounter =0
   }
 
   draw()
@@ -102,8 +126,8 @@ function draw(){
 }
 
 function checkCollision(){
-  return piece.shape.find((row,x)=>{
-    return row.find((value,y)=>{
+  return piece.shape.find((row,y)=>{
+    return row.find((value,x)=>{
       return(
         value !== 0 &&
         board[y+piece.position.y]?.[x+piece.position.x]!== 0
@@ -113,15 +137,23 @@ function checkCollision(){
 }
 
 function solidifyPiece(){
-  piece.shape.forEach((row,x)=>{
-    row.forEach((value,y)=>{
+  piece.shape.forEach((row,y)=>{
+    row.forEach((value,x)=>{
         if(value === 1){
           board[y+piece.position.y][x+piece.position.x] = 1
         }
     })
   })
-  piece.position.x = 3
-  piece.position.y = 2
+  //get random shape and randon position
+  piece.shape = piecesShapes[Math.floor(Math.random()* piecesShapes.length)]
+  //get random position
+  piece.position.x = Math.floor(Math.random() * BOARD_WIDTH/2)
+  piece.position.y = 0
+  //check is game over
+  if(checkCollision()){
+    window.alert("Game Over")
+    board.forEach(row => row.fill(0))
+  }
 }
 
 function removeRows(){
